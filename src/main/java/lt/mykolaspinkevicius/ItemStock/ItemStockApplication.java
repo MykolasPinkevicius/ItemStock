1273fffa08b9c2be831dd98ee2b8184119b3284b
mykolas.pinkevicius <mdpbeats@gmail.com>
2021-01-25 12:20:31 +0200

    initial commit

---
+++ src/main/java/lt/mykolaspinkevicius/ItemStock/ItemStockApplication.java
@@ -0,0 +1,13 @@
+package lt.mykolaspinkevicius.ItemStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/mykolaspinkevicius/ItemStock/ItemStockApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.mykolaspinkevicius.ItemStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
